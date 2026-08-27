fc9a0b6caf393c7f0553afc596080cf78773761a
PracticanteDesarrolloVV <practicantedes@verdevalle.com>
2026-08-27 12:19:39 -0600

    feat(app): configura tema global y navegacion principal

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { ConfigProvider } from 'antd';
+import App from './App.jsx'; 
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ConfigProvider
+    theme={{
+      token: {borderRadius: 8},
+        }}>
+      <App />
+    </ConfigProvider>
   </StrictMode>,
 )
+
---
+++ src/pages/ReservarLugar.jsx
@@ -0,0 +1,4 @@
+function ReservarLugar(){
+    return <h1>Reservar Lugar</h1>
+}
+export default ReservarLugar;
